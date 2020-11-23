af3ab256d29a25961109b5464147ac39d7f553db
Λ B H I N Λ V <ixd.abhi@gmail.com>
2020-11-23 14:43:50 -0500

    event source 600jjk

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 let sse = new EventSource("https://pixelxniffer.herokuapp.com/updates");
 sse.onmessage = (res) => {
     for (id of res.data) {
+        console.log(id);
         document.getElementById("data-container").textContent = id;
     }
 };
