bd5028efde8e469e143a85a93475cf5cd6e3c77d
Λ B H I N Λ V <ixd.abhi@gmail.com>
2020-11-23 15:23:36 -0500

    converted string in to array and used unordered list display the idsf

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,6 @@ let sse = new EventSource("https://pixelxniffer.herokuapp.com/updates");
 sse.onmessage = (res) => {
     let elm = document.getElementById("myUl")
     let data = res.data.split(",")
-    console.log(data);
     elm.innerHTML = "";
     data.forEach(function (item) {
         var li = document.createElement("li");
