db8de8cb91cda31847d9c71a7e9e4aea135f619d
Λ B H I N Λ V <ixd.abhi@gmail.com>
2020-11-23 12:56:15 -0500

    event source 6

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
 let sse = new EventSource("https://pixelxniffer.herokuapp.com/updates");
 sse.onmessage = (res) => {
-    console.log('new Data', res.data);
-    $('#data-container').text(res.data);
+    document.getElementById("data-container").textContent = res.data;
 };
