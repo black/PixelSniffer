ead6f0280cd0a6ad4687ec2f17b20c47fd514682
Λ B H I N Λ V <ixd.abhi@gmail.com>
2020-11-23 14:59:25 -0500

    converted string in to array and used unordered list display the ids

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,14 @@
-let sse = new EventSource("https://pixelxniffer.herokuapp.com/updates");
+// let sse = new EventSource("https://pixelxniffer.herokuapp.com/updates");
+let sse = new EventSource("http://localhost:1000/updates");
 sse.onmessage = (res) => {
-    for (id of res.data) {
-        console.log(id);
-        document.getElementById("data-container").textContent = id;
-    }
+    let elm = document.getElementById("myUl")
+    let data = res.data.split(",")
+    console.log(data);
+    elm.innerHTML = "";
+    data.forEach(function (item) {
+        var li = document.createElement("li");
+        var text = document.createTextNode(item);
+        li.appendChild(text);
+        elm.appendChild(li);
+    });
 };
